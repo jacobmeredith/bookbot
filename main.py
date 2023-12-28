cf683e8a53b18735052486686a66a3080b2410a4
Jacobmeredith <jacobmeredith@hotmail.co.uk>
2023-12-28 17:20:38 +0000

    add report function

--- main.py
+++ main.py
@@ -13,9 +13,24 @@ def get_character_count(contents):
 
     return letters
 
+def print_report(word_count, character_count):
+    print(f"--- Begin report of {file_name} ---")
+    print(f"{word_count} words found in the document\n")
+
+    character_list = sorted(character_count.items(), key=lambda character: character[1], reverse=True)
+
+    for char, count in character_list:
+        if not char.isalpha():
+            continue
+        print(f"The '{char}' character was found {count} times")
+
+    print("--- End report ---")
+
 with open(file_name) as f:
     file_contents = f.read()
     
-    print(f"Word count: {get_word_count(file_contents)}")
-    print(f"Character count: {get_character_count(file_contents)}")
+    word_count = get_word_count(file_contents)
+    character_count = get_character_count(file_contents)
+
+    print_report(word_count, character_count)
 
